96cbc85367f3108a673072c30e3746bcbecea503
Angel Raya <67213108+cyberaya@users.noreply.github.com>
2020-08-20 04:53:02 -0500

    Add files via upload

---
+++ LIsts.py
@@ -0,0 +1,9 @@
+# A list is a collection of items in a particular order. You can make a list that
+# includes the letters of the alphabet, the digits from 0–9, or the names of
+# all the people in your family.
+
+# In Python, square brackets ( [] ) indicate a list, and individual elements
+# in the list are separated by commas.
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
