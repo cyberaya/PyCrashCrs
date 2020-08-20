4acaa57a56470d21b8a55bcab1001087cc4acea8
cyberaya <cyberaya@protonmail.com>
2020-08-19 23:12:34 -0500

    Variables and data types

---
+++ venv/Chapter2/Variables and DataTypes.py
@@ -0,0 +1,16 @@
+first_name = "Ada"
+last_name = "Love"
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
+#print ("\n")
+
+print(f"\n\tHello, {full_name.title()}!")
+
+#_____________________________________________________
+#Removing whitespace
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print (favorite_language)
+
+
