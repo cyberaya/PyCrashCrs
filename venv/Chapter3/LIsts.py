2a1106449274fefdac9b7cbb358abaded7eb8830
cyberaya <cyberaya@protonmail.com>
2020-08-20 08:09:41 -0500

    chapter3 updated

--- venv/Chapter3/LIsts.py
+++ venv/Chapter3/LIsts.py
@@ -1,9 +1,7 @@
 # A list is a collection of items in a particular order. You can make a list that
-# includes the letters of the alphabet, the digits from 0–9, or the names of
-# all the people in your family.
+# includes the letters of the alphabet, the digits from 0–9, or the names of all the people in your family.
 
-# In Python, square brackets ( [] ) indicate a list, and individual elements
-# in the list are separated by commas.
+# In Python, square brackets ( [] ) indicate a list, and individual elements in the list are separated by commas.
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
