e6fd4be078348e7e0e794c5bf0f2141c6b57105b
cyberaya <cyberaya@protonmail.com>
2020-08-21 04:42:22 -0500

    chapter4 updated

--- venv/Chapter4/range2MakeLIstsofNums.py
+++ venv/Chapter4/range2MakeLIstsofNums.py
@@ -57,3 +57,18 @@ print(max(digits))
 # Output is 9
 print(sum(digits))
 # Total sum of digits in list is- 45
+
+#__________ LIST COMPREHENSIONS____________
+
+# A list comprehension allows you to generate this same list in just one line of code!
+# A list comprehension combines the for loop and the creation of new elements into one line, and automatically
+# appends each new element.
+
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+# Notice that no colon is used at the end of the for statement.
+# The result is the same list of square numbers you saw earlier...
+
+cubes = [value**3 for value in range(1,11)]
+print(cubes)
