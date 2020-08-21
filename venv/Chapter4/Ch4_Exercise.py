e1d95dd04d28d15e8a38c3232a8db4f245346def
cyberaya <cyberaya@protonmail.com>
2020-08-21 04:44:45 -0500

    Chapter4 Exercises..

--- venv/Chapter4/Ch4_Exercise.py
+++ venv/Chapter4/Ch4_Exercise.py
@@ -1,17 +1,4 @@
-#__________ LIST COMPREHENSIONS____________
 
-# A list comprehension allows you to generate this same list in just one line of code!
-# A list comprehension combines the for loop and the creation of new elements into one line, and automatically
-# appends each new element.
-
-squares = [value**2 for value in range(1,11)]
-print(squares)
-
-# Notice that no colon is used at the end of the for statement.
-# The result is the same list of square numbers you saw earlier...
-
-cubes = [value**3 for value in range(1,11)]
-print(cubes)
 
 #______________EXERCISES__________________
 # Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive.
