1d1e190193634acd120d93e0b3633a992013382b
cyberaya <cyberaya@protonmail.com>
2020-08-21 04:36:18 -0500

    chapter3 updated

--- venv/Chapter4/list_Comprehensions.py
+++ venv/Chapter4/list_Comprehensions.py
@@ -20,8 +20,21 @@ for digits in twenty:
     print(digits)
 
 
+# One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers.
 millions =list(range(1,1_000_001))
 for mils in millions:
     print(mils)
+print("\n")
 
 
+# Summing a Million:
+# Make a list of the numbers from one to one million
+# Then use min() and max() to make sure your list actually starts at one and ends at one million.
+# Also, use the sum() function to see how quickly Python can add a million numbers.
+sum_ova_million = list(range(1, 1_000_001))
+print("This is the smallest number in the list.")
+print(min(sum_ova_million),"\n")
+print("This is the largest number in the list.")
+print(max(sum_ova_million),"\n")
+print("This is the total sum of the list.")
+print(sum(sum_ova_million),"\n")
