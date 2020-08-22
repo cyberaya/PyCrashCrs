e0d70cd77e728a6775243dcc6d3a0489081783e7
cyberaya <cyberaya@protonmail.com>
2020-08-22 05:03:56 -0500

    chapter3 updated

--- Chapter5/If_Statements.py
+++ Chapter5/If_Statements.py
@@ -1,1 +1,42 @@
-print('CHAPTER 5!')
+print('CHAPTER 5!\n')
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+print(cars, '\n')
+
+# CONDITIONAL TESTS
+# At the heart of every if statement is an expression that can be evaluated as True or False, called a conditional test.
+# Python uses the values True and False to decide whether the code in an if statement should be executed.
+# If a conditional test evaluates to True , Python executes the code following the if statement.
+# If the test evaluates to False , Python ignores the code following the if statement.
+
+car = 'Audi'
+car == 'audi'
+print(car)
+
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print('Hold the anchovies!!\n')
+
+answer = 17
+if answer != 42:
+    print('That is not the correct answer, Try again!')
+
+# You can include various mathematical comparisons in your conditional statements as well
+# Such as less than, less than or equal to, greater than, and greater than or equal to:
+
+age = 19
+if age < 21:
+    print(True)
+if age <= 21:
+    print(True)
+if age > 21:
+    print(False)
+if age >= 21:
+    print(False)
+
