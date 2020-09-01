ec17d6827e2cd42402359350cdad7c6b052e564e
cyberaya <cyberaya@protonmail.com>
2020-09-01 00:11:10 -0500

    Updated

--- Chapter5/if_exercises.py
+++ Chapter5/if_exercises.py
@@ -73,3 +73,31 @@ elif 'red' in alien:
     print('Congrats! You just earned 15 points')
 else:
     print('Sorry, that color is not an option!')
+print('\n')
+
+
+#Stages of Life:
+# Write an if - elif - else chain that determines a person’s stage of life.
+
+# Set a value for the variable age , and then:
+# If the person is less than 2 years old, print a message that the person is a baby.
+# If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+# If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+# If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+# If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+# If the person is age 65 or older, print a message that the person is an elder.
+
+age = 27
+
+if age <=2:
+    print('This is a baby!')
+elif age <=4:
+    print('This is a toddler!')
+elif age <=13:
+    print('This is a kid!')
+elif age <20:
+    print('This is a teenager!')
+elif age <=65:
+    print('This is an adult!')
+elif age <=2:
+    print('This is a baby!')
