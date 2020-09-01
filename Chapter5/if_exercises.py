fbbc4a713526575f896615e5d556e83ac5b3c59d
cyberaya <cyberaya@protonmail.com>
2020-08-31 23:29:05 -0500

    Updated

--- Chapter5/if_exercises.py
+++ Chapter5/if_exercises.py
@@ -37,3 +37,39 @@ if 'green' in alien_color2:
 else:
     print('Congrats! You just earned 10 points')
 
+#Alien Colors #3:
+# Turn your if - else chain from Exercise 5-4 into an if - elif -else chain.
+# If the alien is green, print a message that the player earned 5 points.
+# If the alien is yellow, print a message that the player earned 10 points.
+# If the alien is red, print a message that the player earned 15 points.
+# Write three versions of this program, making sure each message is printed for the appropriate color alien
+
+alien = 'green'
+if 'green' in alien:
+    print('\nCongrats! You just earned 5 points!')
+elif 'yellow' in alien:
+    print('Congrats! You just earned 10 points')
+elif 'red' in alien:
+    print('Congrats! You just earned 15 points')
+else:
+    print('Sorry, that color is not an option!')
+
+alien = 'yellow'
+if 'green' in alien:
+    print('\nCongrats! You just earned 5 points!')
+elif 'yellow' in alien:
+    print('Congrats! You just earned 10 points')
+elif 'red' in alien:
+    print('Congrats! You just earned 15 points')
+else:
+    print('Sorry, that color is not an option!')
+
+alien = 'red'
+if 'green' in alien:
+    print('\nCongrats! You just earned 5 points!')
+elif 'yellow' in alien:
+    print('Congrats! You just earned 10 points')
+elif 'red' in alien:
+    print('Congrats! You just earned 15 points')
+else:
+    print('Sorry, that color is not an option!')
