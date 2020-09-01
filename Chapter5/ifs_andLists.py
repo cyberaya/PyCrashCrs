2981db8a135854a133d7a8c6c891e562657dd129
cyberaya <cyberaya@protonmail.com>
2020-09-01 01:14:51 -0500

    Updated

--- Chapter5/ifs_andLists.py
+++ Chapter5/ifs_andLists.py
@@ -21,4 +21,29 @@ for requested_topping in requested_toppings:
     else:
         print(f'Adding {requested_topping}.')
 
+print('\nFinished making your pizza!\n')
+
+print('CHECKING THAT A LIST IS NOT EMPTY\n')
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f'Adding {requested_topping}')
+        print('\nFinished making your pizza!')
+else:
+    print('Are you sure you want a plain pizza?')
+
+print('\nUSING MULTIPLE LISTS_________________\n')
+
+available_toppings = ['mushrooms','olives','green peppers','pepperoni','pineapple','extra cheese']
+
+requested_toppings = ['mushrooms','french fries','extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f'Adding {requested_topping}...')
+    else:
+        print(f'Sorry, we do not have {requested_topping}.')
+
 print('\nFinished making your pizza!')
